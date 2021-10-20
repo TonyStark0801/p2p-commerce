ba1e4ce30a45d4bc4578f89772fc23c0f8309715
sigmaducc <adnan.ch09@protonmail.com>
2021-10-21 04:47:18 +0530

    Manually merged conflicts from Shubham's pull request

---
+++ server/src/index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import env from "dotenv";
+import mongoose from "mongoose";
+import router from "../src/routes/user.js";
+const url = `mongodb+srv://nsa:nsa%402021@cluster0.dbkqe.mongodb.net/p2p_ecommerce?retryWrites=true&w=majority`;
+//const url = "mongodb://localhost/p2p_ecommerce";
+env.config();
+
+//Routes
+
+//Database connection
+mongoose
+  .connect(url)
+  .then(() => {
+    console.log("Database Connected");
+  })
+  .catch((err) => console.log(err));
+
+//Middlewares
+const app = express();
+app.use(express.json());
+app.use("/api", router);
+
+//Listen
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on ${process.env.PORT}`);
+});
