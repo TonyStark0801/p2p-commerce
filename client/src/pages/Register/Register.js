87e9f90280875ff8ada00672d13d2c4b74e30db7
Adnan <adnan.ch09@protonmail.com>
2022-08-05 02:38:11 +0530

    fixing vercel deployment issyes

--- client/src/pages/Register/Register.js
+++ client/src/pages/Register/Register.js
@@ -7,7 +7,7 @@ function Register(props) {
   const [password, setPassword] = useState("");
   function submitHandler(e) {
     e.preventDefault();
-    axios.post("http://localhost:5000/api/signup", {
+    axios.post("https://p2p-comm-server.herokuapp.com/api/signup", {
       username: username,
       email: email,
       password: password,
